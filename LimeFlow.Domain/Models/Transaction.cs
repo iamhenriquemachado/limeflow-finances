8ffe153c1ce7291f709d83348e2dc40fe8186335
henrique.castro <heyhenriquecastro@gmail.com>
2026-06-17 20:30:26 -0300

    Refactor Transaction type to use enum
    Added a `TransactionType` enum in the `LimeFlow.Domain.Enums` namespace to represent transaction types (`Income` and `Expense`).
    Updated the `Transaction` class to replace the `Type` property from `int` to the new `TransactionType` enum.
    Modified `Transaction.cs` to include the necessary namespace reference for the enum.

--- LimeFlow.Domain/Models/Transaction.cs
+++ LimeFlow.Domain/Models/Transaction.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LimeFlow.Domain.Enums;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,7 +14,7 @@ namespace LimeFlow.Domain.Models
         public string Description { get; set; }
         public string Category { get; set; }
         public string Account { get; set; }
-        public int Type { get; set; }
+        public TransactionType Type { get; set; }
         public decimal Amount { get; set; }
 
     }
