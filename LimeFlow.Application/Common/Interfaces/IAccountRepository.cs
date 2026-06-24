796dca5d066a919d520a6249f81a43e65f6dc2e0
henrique.castro <heyhenriquecastro@gmail.com>
2026-06-23 21:07:04 -0300

    Add Account entity, repository, and migration
    Introduced the `Account` entity with its associated repository interface (`IAccountRepository`) for CRUD operations. Added a new migration (`20260623235608_AddAccount`) to define the database schema for `Account` and other related entities.
    
    Created `AccountConfiguration` to configure the `Account` entity, including its primary key and relationship with the `User` entity (one-to-many with cascading delete). Updated `UserConfiguration` to enforce `CreatedAt` and `LastUpdatedAt` as required fields.
    
    These changes expand the domain model to include accounts linked to users.

---
+++ LimeFlow.Application/Common/Interfaces/IAccountRepository.cs
@@ -0,0 +1,18 @@
+﻿using LimeFlow.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LimeFlow.Application.Common.Interfaces
+{
+    public interface IAccountRepository
+    {
+        Task CreateAsync(Account account);
+        Task UpdateAsync(Guid id, Account account);
+        Task DeleteAsync(Guid id);
+        Task<Account> GetByIdAsync(Guid id);
+        Task<IReadOnlyList<Account>> GetAllAsync();
+    }
+}
---
+++ LimeFlow.Infrastructure/Persistence/Configurations/AccountConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using LimeFlow.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LimeFlow.Infrastructure.Persistence.Configurations
+{
+    internal class AccountConfiguration
+    {
+        public void Configure(EntityTypeBuilder<Account> builder)
+        {
+            builder.HasKey(a => a.Id);
+
+            builder.HasOne<User>()
+                   .WithMany()
+                   .HasForeignKey(a => a.UserId)
+                   .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
