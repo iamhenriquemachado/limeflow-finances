62246390982782c17393db400cb135f2c33fb08f
henrique.castro <heyhenriquecastro@gmail.com>
2026-06-26 19:02:35 -0300

    Add DeleteUserCommand and its handler
    Added the `DeleteUserCommand` record to represent a command for deleting a user, identified by a `Guid` parameter.
    
    Implemented the `DeleteUserCommandHandler` class, which handles the `DeleteUserCommand`. The handler depends on an `IUserRepository` and includes a constructor for dependency injection. The `Handle` method is defined but not yet implemented.

---
+++ LimeFlow.Application/Mediator/Users/Commands/DeleteUserCommand.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LimeFlow.Application.Mediator.Users.Commands
+{
+    public record DeleteUserCommand(Guid id);
+}
---
+++ LimeFlow.Application/Mediator/Users/Handlers/DeleteUserCommandHandler.cs
@@ -0,0 +1,25 @@
+﻿using LimeFlow.Application.Common.Interfaces;
+using LimeFlow.Application.Mediator.Users.Commands;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LimeFlow.Application.Mediator.Users.Handlers
+{
+    public class DeleteUserCommandHandler : IRequestHandler<DeleteUserCommand>
+    {
+        private readonly IUserRepository _repo;
+        public DeleteUserCommand(IUserRepository repo)
+        {
+            _repo = repo;
+        }
+
+        public async Task Handle(DeleteUserCommand request, Guid id)
+        {
+
+        }
+    }
+}
