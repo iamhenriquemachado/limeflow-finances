35c476b633587172f2a5b6549b40a88afa228ec9
henrique.castro <heyhenriquecastro@gmail.com>
2026-06-27 11:47:34 -0300

    Add AccountRepository implementation skeleton
    A new `AccountRepository` class has been added to the
    `LimeFlow.Infrastructure.Repositories` namespace. This class
    implements the `IAccountRepository` interface and includes
    method stubs for creating, deleting, retrieving, and updating
    accounts. All methods currently throw `NotImplementedException`.
    
    The class depends on `AppDbContext`, which is injected via
    its constructor. Necessary `using` directives have been added
    to support the implementation.

---
+++ LimeFlow.Infrastructure/Repositories/AccountRepository.cs
@@ -0,0 +1,45 @@
+﻿using LimeFlow.Application.Common.Interfaces;
+using LimeFlow.Domain.Models;
+using LimeFlow.Infrastructure.Database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LimeFlow.Infrastructure.Repositories
+{
+    public class AccountRepository : IAccountRepository
+    {
+        private readonly AppDbContext _context;
+
+        public AccountRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+        public Task CreateAsync(Account account)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IReadOnlyList<Account>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Account> GetByIdAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateAsync(Guid id, Account account)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
