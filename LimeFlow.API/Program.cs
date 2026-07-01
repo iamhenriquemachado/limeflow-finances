ac1355f21dd8ef2bcf27b0df0735f5ff1b7a26bc
henrique.castro <heyhenriquecastro@gmail.com>
2026-07-01 20:46:59 -0300

    Add JWT Authentication and Refactor TokenService
    Implemented JWT authentication middleware in `Program.cs` with token validation parameters. Introduced `JwtSettings` class for managing JWT configuration and registered it in the DI container. Refactored `TokenService` to use `JwtSettings` and updated `ITokenService` interface accordingly. Added `Microsoft.AspNetCore.Authentication.JwtBearer` package to support JWT functionality. Updated project files and cleaned up redundant code and imports.

--- LimeFlow.API/Program.cs
+++ LimeFlow.API/Program.cs
@@ -5,10 +5,29 @@ using LimeFlow.Application.Common.Interfaces;
 using LimeFlow.Application.Services;
 using LimeFlow.Infrastructure.Database;
 using LimeFlow.Infrastructure.Repositories;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Scalar.AspNetCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(builder.Configuration["JwtSettings:Secret"]!)),
+        ValidateIssuer = true, 
+        ValidateAudience = true, 
+        ValidIssuer = builder.Configuration["JwtSettings:Issuer"], 
+        ValidAudience = builder.Configuration["JwtSettings:Audience"]
+    };
+});
+
+builder.Services.AddAuthorization();
+
 
 builder.Services.AddOpenApi();
 builder.Services.AddProblemDetails();
@@ -24,6 +43,8 @@ builder.Services.AddScoped<IAccountService, AccountService>();
 
 var app = builder.Build();
 
+app.UseAuthentication();
+app.UseAuthorization();
 app.UseExceptionHandler();
 app.UseMiddleware<ValidationExceptionMiddleware>();
 app.MapOpenApi();
--- LimeFlow.Infrastructure/Auth/TokenService.cs
+++ LimeFlow.Infrastructure/Auth/TokenService.cs
@@ -1,29 +1,45 @@
 ﻿using Microsoft.Extensions.Configuration;
-using System;
-using System.Collections.Generic;
-using System.Linq;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using LimeFlow.Domain.Models.Settings;
+using Microsoft.Extensions.Options;
+using LimeFlow.Domain.Models;
 using System.Text;
-using System.Threading.Tasks;
+using Microsoft.IdentityModel.Tokens;
+using System.Security.Claims;
+using System.IdentityModel.Tokens.Jwt;
 
 namespace LimeFlow.Infrastructure.Auth
 {
     public class TokenService : ITokenService
     {
+        private readonly JwtSettings _settings;
 
-        private readonly IConfiguration _configuration;
 
-        public TokenService(IConfiguration configuration)
+        public TokenService(IOptions<JwtSettings> settings) => _settings = settings.Value;
+        public string GenerateToken(User user)
         {
-            _configuration = configuration;
-        }
-        public string GenerateToken(string userId, string email, IEnumerable<string> roles)
-        {
-            
+            var key = Encoding.ASCII.GetBytes(_settings.Secret);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new[]
+                {
+                    new Claim(ClaimTypes.Name, user.Email)
+                }),
+                Expires = DateTime.UtcNow.AddHours(1),
+                SigningCredentials = new SigningCredentials(
+                    new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature),
+                Issuer = _settings.Issuer,
+                Audience = _settings.Audience
+            };
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            return tokenHandler.CreateToken(tokenDescriptor).ToString();
+
         }
     }
 
     public interface ITokenService
     {
-        string GenerateToken(string userId, string email, IEnumerable<string> roles);
+        string GenerateToken(User user);
     }
 }
