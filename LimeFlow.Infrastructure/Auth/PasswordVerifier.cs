e0c5b25933ab7b208519b45b8219b2f08e6c7b3c
henrique.castro <heyhenriquecastro@gmail.com>
2026-07-05 16:04:07 -0300

    Add PasswordVerifier class and IPasswordVerifier interface
    Introduced the `PasswordVerifier` class and `IPasswordVerifier`
    interface in the `LimeFlow.Infrastructure.Auth` namespace.
    The `PasswordVerifier` class implements the `VerifyPassword`
    method to validate passwords using `BCrypt.Net.BCrypt.EnhancedVerify`.
    
    Added necessary `using` directives for required namespaces.
    Note: The `VerifyPassword` method currently lacks a `return`
    statement, which needs to be addressed to avoid compilation errors.

---
+++ LimeFlow.Infrastructure/Auth/PasswordVerifier.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LimeFlow.Infrastructure.Auth
+{
+    public class PasswordVerifier : IPasswordVerifier
+    {
+        public Task<bool> VerifyPassword(string password, string storedPasswordHashed)
+        {
+            bool passwordMatches = BCrypt.Net.BCrypt.EnhancedVerify(password, storedPasswordHashed);
+
+
+
+        }
+    }
+
+    public interface IPasswordVerifier
+    {
+        Task<bool> VerifyPassword(string password);
+    }
+}
